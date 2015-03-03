fc134dbdfd08b0426be59a8b2fd854f351e66bdd
Wu Ke <wke918@aucklanduni.ac.nz>
2015-03-04 12:34:33 +1300

    使用web service实例，中英翻译

---
+++ Default.aspx
@@ -0,0 +1,20 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <link href="main.css" rel="stylesheet" />
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div class="assistant">
+        <h3>英汉双向翻译</h3>
+        输入单词:<asp:TextBox ID="word" runat="server"></asp:TextBox>
+        <asp:Button ID="Button1" runat="server" Text="翻译" OnClick="Button1_Click" />
+        <asp:Label ID="translation" runat="server"></asp:Label>
+    </div>
+    </form>
+</body>
+</html>
---
+++ Default.aspx.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        cn.com.webxml.fy.EnglishChinese utility = new cn.com.webxml.fy.EnglishChinese();
+        try
+        {
+            string[] result = utility.TranslatorString(word.Text);
+            string temp = "";
+            temp += "读音：" + result[1] + "<br/>";
+            temp += "翻译:" + result[3] + "<br/>";
+            translation.Text = temp;
+        }
+        catch
+        {
+            translation.Text = "翻译过程中出现错误";
+        }
+        finally
+        {
+            utility.Dispose();
+        }
+    }
+}
