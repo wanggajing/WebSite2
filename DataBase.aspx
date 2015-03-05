c94576e749f4ba680cc1b348bc7db5bc3369711b
Wu Ke <wke918@aucklanduni.ac.nz>
2015-03-05 17:38:30 +1300

    ADO.NET

---
+++ DataBase.aspx
@@ -0,0 +1,55 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="DataBase.aspx.cs" Inherits="DataBase" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Connect" />
+        <br />
+        <asp:Label ID="Label1" runat="server" Text="输出信息"></asp:Label>
+        <br />
+        UserName:<asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+        <br />
+        Password<asp:TextBox ID="TextBox2" runat="server"></asp:TextBox>
+        <br />
+        <asp:Button ID="Button2" runat="server" OnClick="Button2_Click" Text="注册" />
+    
+        <br />
+        <br />
+        <asp:Label ID="Label2" runat="server" Text="修改密码"></asp:Label>
+        <br />
+        用户名:<asp:TextBox ID="TextBox3" runat="server"></asp:TextBox>
+        <br />
+        原密码:<asp:TextBox ID="TextBox4" runat="server"></asp:TextBox>
+        <br />
+        新密码:<asp:TextBox ID="TextBox5" runat="server"></asp:TextBox>
+        <br />
+        <asp:Button ID="Button3" runat="server" OnClick="Button3_Click" Text="修改密码" />
+    
+        <br />
+        <br />
+        <asp:Button ID="Button4" runat="server" Text="统计用户人数" OnClick="Button4_Click" />
+        <br />
+        <asp:Label ID="Total" runat="server" Text="Label"></asp:Label>
+        <br />
+        <asp:Label ID="Male" runat="server" Text="Label"></asp:Label>
+        <br />
+        <asp:Label ID="Female" runat="server" Text="Label"></asp:Label>
+    
+        <br />
+        <br />
+        <asp:Button ID="Button5" runat="server" Text="查询" OnClick="Button5_Click" />
+        <br />
+        <asp:Table ID="Result" runat="server" BorderStyle="Solid" BorderWidth="1px" GridLines="Both">
+        </asp:Table>
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ DataBase.aspx.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class DataBase : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        string connString = ConfigurationManager.ConnectionStrings["database"].ConnectionString;
+        Label1.Text = "";
+        using (SqlConnection conn = new SqlConnection(connString))
+        {
+            conn.Open();
+            Label1.Text += "连接已近打开<br />";
+            conn.Close();
+            Label1.Text += "连接已近关闭<br />";
+        }
+    }
+    protected void Button2_Click(object sender, EventArgs e)
+    {
+        string connString = ConfigurationManager.ConnectionStrings["database"].ConnectionString;
+        SqlConnection conn = new SqlConnection(connString);
+        string sql = "insert into UserTable (UserName,Password) values (@name, @pass)";
+        SqlCommand command = new SqlCommand(sql, conn);
+        SqlParameter p1 = new SqlParameter("@name", TextBox1.Text);
+        command.Parameters.Add(p1);
+        SqlParameter p2 = new SqlParameter("@pass", TextBox2.Text);
+        command.Parameters.Add(p2);
+        string message = "";
+        try
+        {
+            conn.Open();
+            int n = command.ExecuteNonQuery();
+            if (n == 1)
+                message = "注册成功。用户信息已经保存在数据库";
+            else
+                message = "数据未能保存";
+        }
+        catch (Exception ex)
+        {
+            message = "保存用户信息过程中出错" + ex.Message.Replace("\r", "\\r ").Replace("\n", "\\n");
+        }
+        finally
+        {
+            command.Dispose();
+            conn.Close();
+        }
+        Page.ClientScript.RegisterStartupScript(this.GetType(), "register", "<script>alert(\"" + message + "\");</script>");
+    }
+    protected void Button3_Click(object sender, EventArgs e)
+    {
+        string connString = ConfigurationManager.ConnectionStrings["database"].ConnectionString;
+        SqlConnection conn = new SqlConnection(connString);
+        string sql = "update UserTable set Password=@newpass " + "where UserName=@name and Password=@oldpass";
+        string sql1 = "update UserTable set Password='12344' where UserName='wuke' and Password='123'";
+        SqlCommand command = new SqlCommand(sql1, conn);
+    /*    SqlParameter p1 = new SqlParameter("@name", TextBox3.Text);
+        command.Parameters.Add(p1);
+        SqlParameter p2 = new SqlParameter("@oldpass", TextBox4.Text);
+        command.Parameters.Add(p2);
+        SqlParameter p3 = new SqlParameter("@newpass", TextBox5.Text);
+        command.Parameters.Add(p3);
+     */ 
+
+        string message = "";
+        try
+        {
+            conn.Open();
+            int n = command.ExecuteNonQuery();
+            if (n == 1)
+                message = "密码修改成功";
+            else
+                message = "不存在此用户或原始密码不对";
+        }
+        catch (Exception ex)
+        {
+            message = "更改密码过程中出错" + ex.Message.Replace("\r", "\\r ").Replace("\n", "\\n");
+        }
+        finally
+        {
+            command.Dispose();
+            conn.Close();
+        }
+        Page.ClientScript.RegisterStartupScript(this.GetType(), "register", "<script>alert(\"" + message + "\");</script>");
+    }
+    protected void Button4_Click(object sender, EventArgs e)
+    {
+        string connString = ConfigurationManager.ConnectionStrings["database"].ConnectionString;
+        SqlConnection conn = new SqlConnection(connString);
+        string sql = "select count(*) from UserTable";
+        SqlCommand command = new SqlCommand(sql, conn);
+        string message = "";
+        try
+        {
+            conn.Open();
+            int n = Convert.ToInt32(command.ExecuteScalar());
+            Total.Text = n.ToString();
+        }
+        catch (Exception ex)
+        {
+            message = "执行命令过程中出错" + ex.Message.Replace("\r", "\\r ").Replace("\n", "\\n");
+        }
+        finally
+        {
+            command.Dispose();
+            conn.Close();
+        }
+        Page.ClientScript.RegisterStartupScript(this.GetType(), "register", "<script>alert(\"" + message + "\");</script>");
+    }
+    protected void Button5_Click(object sender, EventArgs e)
+    {
+        string connString = ConfigurationManager.ConnectionStrings["database"].ConnectionString;
+        using (SqlConnection conn = new SqlConnection(connString))
+        {
+            string sql = "select UserId,UserName,Password,Sex from UserTable";
+            SqlCommand command = new SqlCommand(sql, conn);
+            conn.Open();
+            using (SqlDataReader reader = command.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    TableRow row = new TableRow();
+                    Result.Rows.Add(row);
+                    for (int i = 0; i < 3; i++)
+                    {
+                        TableCell cell = new TableCell();
+                        cell.Text = Convert.ToString(reader[i]);
+                        row.Cells.Add(cell);
+                    }
+                }
+            }
+            command.Dispose();
+            conn.Close();
+        }
+    }
+}
