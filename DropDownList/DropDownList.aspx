7648b4902f9ad208cb976293b54308fdec249356
Wu Ke <wke918@aucklanduni.ac.nz>
2015-03-07 13:08:41 +0800

    DropDownList控件二

--- DropDownList/DropDownList.aspx
+++ DropDownList/DropDownList.aspx
@@ -5,11 +5,22 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head runat="server">
     <title></title>
+    <script>
+        function showSelection() {
+            var selection = document.getElementById('list');
+            if (selection.selectedIndex >= 0) {
+                var option = selection.options[selection.selectedIndex];
+                var msg = "你选的职业是： " + option.text + "职业代码是： " + option.value;
+                alert(msg);
+            }
+        }
+    </script>
 </head>
 <body>
     <form id="form1" runat="server">
     <div>
-        <asp:DropDownList ID="list" runat="server" DataValueField="OccupationId" DataTextField="OccupationName" onchange="showSelection()"></asp:DropDownList><br />
+        <asp:DropDownList ID="list" runat="server" DataValueField="OccupationId" 
+            DataTextField="OccupationName" onchange="showSelection()"></asp:DropDownList><br />
     </div>
     </form>
 </body>
--- DropDownList/DropDownList.aspx.cs
+++ DropDownList/DropDownList.aspx.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -9,6 +11,22 @@ public partial class DropDownList_DropDownList : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-
+        if (!IsPostBack)
+        {
+            bindOccupationList();
+        }
+    }
+    private void bindOccupationList()
+    {
+        String s = System.Configuration.ConfigurationManager.ConnectionStrings["database2"].ConnectionString;
+        SqlConnection connection = new SqlConnection(s);
+        string sql = "select * from Occupation";
+        SqlDataAdapter adapter = new SqlDataAdapter(sql, s);
+        DataTable table = new DataTable();
+        adapter.FillSchema(table, SchemaType.Source);
+        adapter.Fill(table);
+        list.DataSource = table;
+        list.DataBind();
+        table.Dispose();
     }
 }
