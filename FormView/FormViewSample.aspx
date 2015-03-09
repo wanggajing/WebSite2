4914edf31ba7dd551f77be415087b26d7a636006
Wu Ke <wke918@aucklanduni.ac.nz>
2015-03-09 17:22:34 +1300

    FromView显示，编辑，删除数据

---
+++ FormView/FormViewSample.aspx
@@ -0,0 +1,67 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="FormViewSample.aspx.cs" Inherits="FormView_FormViewSample" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <asp:FormView ID="FormView1" runat="server" DataKeyNames="ProductId" OnPageIndexChanging="FormView1_PageIndexChanging"
+             OnModeChanging="FormView1_ModeChanging" OnItemDeleting="FormView1_ItemDeleting" 
+            OnItemUpdating="FormView1_ItemUpdating" AllowPaging="true">
+            <ItemTemplate>
+                <table>
+                    <tr>
+                        <td>
+                            <img src="../ProductImages/<%#Eval("ImageUrl") %>" alt="<%#Eval("ProductName") %>" 
+                                style="width:200px" /> 
+                        </td>
+                        <td>
+                            <span style="font-weight:bold"><%#Eval("ProductName") %></span><br />
+                            价格:<%#Eval("Price","{0:C}") %><br />说明:<%#Eval("Description") %><br />库存:<%#Eval("Stock") %></td>
+                        <td>
+                            <asp:LinkButton ID="editButton" runat="server" Text="编辑" CommandName="edit" />
+                        </td>
+                        <td>
+                            <asp:LinkButton ID="deleteButton" runat="server" Text="删除" CommandName="delete"
+                                OnClientClick="return confirm('确实要删除吗?');" />
+                        </td>
+                    </tr>
+                </table>
+            </ItemTemplate>
+            <EditItemTemplate>
+                <table>
+                    <tr>
+                        <td>
+                            商品图片<br />
+                            <img src="../ProductImages/<%#Eval("ImageUrl") %>" alt="<%#Eval("ProductName") %>" 
+                                style="width:200px" />
+                             <br />
+                            上传新的商品图片:<br />
+                            <asp:FileUpload ID="imageFile" runat="server" /><br />
+                        </td>
+                        <td>
+                            名称:<asp:TextBox ID="productName" runat="server" Text='<%#Eval("ProductName") %>'>
+                               </asp:TextBox><br />
+                            价格:<asp:TextBox ID="productPrice" runat="server" Text='<%#Eval("Price") %>'></asp:TextBox><br />
+                            说明:<asp:TextBox ID="description" runat="server" Text='<%#Eval("Description") %>'></asp:TextBox><br />
+                            库存:<asp:TextBox ID="stock" runat="server" Text='<%#Eval("stock") %>'></asp:TextBox>
+                        </td>
+                        <td>
+                            <asp:LinkButton ID="cancelButton" runat="server" Text="取消" CommandName="cancel" />
+                            <asp:LinkButton ID="updateButton" runat="server" Text="保存" CommandName="update" />
+                        </td>
+                        <td>
+                            <asp:LinkButton ID="deleteButton" runat="server" Text="删除" CommandName="delete" />
+                        </td>
+                    </tr>
+                </table>
+            </EditItemTemplate>
+        </asp:FormView>
+    </div>
+    </form>
+</body>
+</html>
---
+++ FormView/FormViewSample.aspx.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class FormView_FormViewSample : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!IsPostBack)
+        {
+            bindData();
+        }
+    }
+    private void bindData()
+    {
+        string s = System.Configuration.ConfigurationManager.ConnectionStrings["database2"].ConnectionString;
+        SqlConnection connection = new SqlConnection(s);
+        string sql = "select * from Products";
+        SqlDataAdapter adapter = new SqlDataAdapter(sql, connection);
+        DataTable table = new DataTable();
+        adapter.FillSchema(table, SchemaType.Source);
+        adapter.Fill(table);
+        FormView1.DataSource = table;
+        FormView1.DataBind();
+        table.Dispose();
+    }
+    protected void FormView1_PageIndexChanging(object sender, FormViewPageEventArgs e)
+    {
+        FormView1.PageIndex = e.NewPageIndex;
+        bindData();
+    }
+    protected void FormView1_ModeChanging(object sender, FormViewModeEventArgs e)
+    {
+        FormView1.ChangeMode(e.NewMode);
+        bindData();
+    }
+    protected void FormView1_ItemDeleting(object sender, FormViewDeleteEventArgs e)
+    {
+        string key = e.Keys[0].ToString();
+        string sql = "delete from Products where ProductId=@id";
+        string s = System.Configuration.ConfigurationManager.ConnectionStrings["database2"].ConnectionString;
+        SqlConnection connection = new SqlConnection(s);
+        SqlCommand command = new SqlCommand(sql, connection);
+        SqlParameter p1 = new SqlParameter("@id", key);
+        command.Parameters.Add(p1);
+        connection.Open();
+        int i = command.ExecuteNonQuery();
+        FormView1.ChangeMode(FormViewMode.ReadOnly);
+        bindData();
+        connection.Close();
+    }
+    protected void FormView1_ItemUpdating(object sender, FormViewUpdateEventArgs e)
+    {
+        string key = e.Keys[0].ToString();
+        string productName = (FormView1.FindControl("productName") as TextBox).Text;
+        double price = double.Parse((FormView1.FindControl("productPrice") as TextBox).Text);
+        double stock = double.Parse((FormView1.FindControl("stock") as TextBox).Text);
+        string description = (FormView1.FindControl("description") as TextBox).Text;
+        string image = null;
+        FileUpload file = FormView1.FindControl("imageFile") as FileUpload;
+        if (file.HasFile)
+        {
+            image = file.FileName;
+            file.SaveAs(Server.MapPath("~/ProductImages/" + file.FileName));
+        }
+        string sql = "update Products set ProductName=@name,Price=@price,Stock=@stock,"+
+            "Description=@description" + (file.HasFile?",ImageUrl=@img ":" ")+" where ProductId=@id";
+        string s = System.Configuration.ConfigurationManager.ConnectionStrings["database2"].ConnectionString;
+        SqlConnection connection = new SqlConnection(s);
+        SqlCommand command = new SqlCommand(sql, connection);
+        SqlParameter p1 = new SqlParameter("@name", productName);
+        SqlParameter p2 = new SqlParameter("@price", price);
+        SqlParameter p3 = new SqlParameter("@stock", stock);
+        SqlParameter p4 = new SqlParameter("@description", description);
+        SqlParameter p5 = null;
+        if (file.HasFile)
+        {
+             p5 = new SqlParameter("@img", image);
+        }    
+        SqlParameter p6 = new SqlParameter("@id", key);
+        command.Parameters.Add(p1);
+        command.Parameters.Add(p2);
+        command.Parameters.Add(p3);
+        command.Parameters.Add(p4);
+        if (file.HasFile)
+        {
+            command.Parameters.Add(p5);
+        }
+        command.Parameters.Add(p6);
+        connection.Open();
+        int i = command.ExecuteNonQuery();
+        FormView1.ChangeMode(FormViewMode.ReadOnly);
+        bindData();
+        connection.Close();
+    }
+}
