c71e990a519366d2356b0681d02c6a107acecb9f
Wu Ke <wke918@aucklanduni.ac.nz>
2015-04-09 14:18:47 +1200

    三层结构银行转账系统2

---
+++ App_Code/DAL/CustomerDAL.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for CustomerDAL
+/// </summary>
+public class CustomerDAL
+{
+    private DBContext context = new DBContext();
+    public Customer GetById(string id)
+    {
+        var customer = context.Customers.Select(c => c.CustomerID == id);
+        return customer as Customer;
+    }
+}
---
+++ App_Code/Model/DBContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for DBContext
+/// </summary>
+public class DBContext: DbContext
+{
+    public DbSet<Customer> Customers { get; set; }
+    public DbSet<BankAccount> BankAccounts { get; set; }
+    public DbSet<AccountTransaction> AccountTransactions { get; set; }
+    public DbSet<BankPolicy> BankPolicy { get; set; }
+}
