7b3ff527da36e7c07b25d738dbbdd4bc39e68a36
Wu Ke <wke918@aucklanduni.ac.nz>
2015-05-27 12:16:40 +0800

    Three layers' banking system, working with test

---
+++ App_Code/BLL/BankAccountBLL.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for BankAccountBLL
+/// </summary>
+public static class BankAccountBLL
+{
+    public static BankAccount GetById(string id)
+    {
+        return BankAccountDAL.GetById(id);
+    }
+    public static void AddMoney(string account, double money)
+    {
+        BankAccount bankAccount = BankAccountDAL.GetById(account);
+        if (bankAccount.IsLocked)
+            throw new Exception("Your account" + account + "has been locked");
+        BankAccountDAL.AddMoney(account, money);
+    }
+}
---
+++ App_Code/DAL/PolicyDAL.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for PolicyDAL
+/// </summary>
+public static class PolicyDAL
+{
+    private const string SINGLE_TRANSFER_LIMIT_ID = "policy001";
+    private const string TRANSFER_TIMES_LIMIT_ID = "policy002";
+    private const string TRANSFER_MONEY_LIMIT_ID = "policy003";
+    private static DBContext context = new DBContext();
+    public static BankPolicy GetById(string id)
+    {
+        var result = context.BankPolicy.Where(p => p.PolicyID == id);
+        return result as BankPolicy;
+    }
+    public static double GetSingTransferLimit()
+    {
+        BankPolicy policy = GetById(SINGLE_TRANSFER_LIMIT_ID);
+        return Convert.ToDouble(policy.PolicyContent);
+    }
+    public static int GetTransferTimesLimit()
+    {
+        BankPolicy policy = GetById(TRANSFER_TIMES_LIMIT_ID);
+        return int.Parse(policy.PolicyContent);
+    }
+    public static double GetTransferMoneyLimit()
+    {
+        BankPolicy policy = GetById(TRANSFER_MONEY_LIMIT_ID);
+        return double.Parse(policy.PolicyContent);
+    }
+}
